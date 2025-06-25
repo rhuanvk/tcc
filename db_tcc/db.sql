1753fea4d688cc4a90d674d2f003978a75082575
Rhuan Klassmann <rhuanvk@gmail.com>
2025-06-25 20:21:52 -0300

    remodelado db

--- db_tcc/db.sql
+++ db_tcc/db.sql
@@ -1,26 +1,23 @@
-create table public.produtos
+create table produtos
 (
-    id                 serial constraint produtos_pk primary key,
+    id                 serial
+        constraint produtos_pk
+            primary key,
     descricao          varchar(100)   not null,
     valor              numeric(10, 2) not null,
     quantidade_estoque integer        not null
 );
 
-create table public.vendas
+create table historico
 (
-    id                 serial constraint vendas_pk primary key,
-    nome_cliente       varchar(100),
-    valor_total        numeric(10, 2) not null,
-    data_venda         timestamp      not null
+    id                integer default nextval('movimentacoes_id_seq'::regclass) not null
+        constraint movimentacoes_pk
+            primary key,
+    id_produto        integer                                                   not null,
+    descricao_produto varchar(100)                                              not null,
+    quantidade        integer                                                   not null,
+    tipo              char                                                      not null,
+    data_hora         timestamp                                                 not null
 );
 
-create table public.movimentacoes
-(
-    id                 serial constraint movimentacoes_pk primary key,
-    id_produto         integer not null constraint movimentacoes_produtos_id_fk references public.produtos,
-    id_venda           integer constraint movimentacoes_vendas_id_fk references public.vendas,
-    quantidade         integer not null,
-    tipo               char    not null
-);
-
-comment on column public.movimentacoes.tipo is 'E = Entrada; S = Saída';
+comment on column historico.tipo is 'C = Cadastro; A = Alteração; E = Exclusão';
